8d672470a3a65fb5fb277784717ca489a50d02c7
Tran Tri Vien <mr.betran.qn@gmail.com>
2021-06-27 00:59:19 +0700

    player

---
+++ Tap To Win/Assets/Scripts/Spawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject virus;
+    public float respawnTime =2.0f;
+    public float screenX;
+    public float minY;
+    public float maxY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(VirusWare());
+    }
+    private void SpawnEnemy()
+    {
+        Debug.Log("ok");
+        GameObject a = Instantiate(virus) as GameObject;
+        a.transform.position = new Vector2(screenX , Random.Range(minY, maxY));
+    }
+
+    // Update is called once per frame
+    IEnumerator VirusWare()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(2);
+            SpawnEnemy();
+        }
+    }
+}
